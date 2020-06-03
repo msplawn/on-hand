ea60f65b798b1fa45deae866fbecffaf3fd31194
msplawn <mmsplawn@gmail.com>
2020-06-02 21:13:46 -0500

    added comments on the js

--- base.js
+++ base.js
@@ -1,3 +1,4 @@
+//The 3 IDs in these functions are for the containers around the three "pages" to be displayed
 function displayHungry()    {
     $('#hungryPage').style('display: flex');
     $('#landingPage').style('display: none');
@@ -16,7 +17,7 @@ function displayThirsty()    {
     $('#thirstyPage').style('display: none');
 }
 
-
+// This is the beginnings of the ajax request
 $('#searchCocktails').on('click', function(event)    {
     event.preventDefault();
 
@@ -33,6 +34,8 @@ $('#searchCocktails').on('click', function(event)    {
       });
 });
 
+//These three IDs refer to the containers for the hungry / thirsty button&icon container 
+//and the landing button in the nav
 $('#hungry').on('click', displayHungry());
 $('#thirsty').on('click', displayThirsty());
 $('#landing').on('click', displayLanding());

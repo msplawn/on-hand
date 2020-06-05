feaf0daed2be7e8314e705c8ca99220b557957d9
Violet Death <ladymariaownsbro@gmail.com>
2020-06-04 22:08:53 -0500

    tutor work

--- base.js
+++ base.js
@@ -1,17 +1,20 @@
 //The 3 IDs in these functions are for the containers around the three "pages" to be displayed
-function displayHungry()    {
+function displayHungry() {
+    console.log("Hungry has been clicked");
     $('#hungryPage').style('display: flex');
     $('#landingPage').style('display: none');
     $('#thirstyPage').style('display: none');
 }
 
-function displayThirsty()    {
+function displayThirsty() {
+    console.log("Thirsty has been clicked")
     $('#thirstyPage').style('display: flex');
     $('#landingPage').style('display: none');
     $('#hungryPage').style('display: none');
 }
 
-function displayThirsty()    {
+function displayLanding() {
+    console.log("Landing has been clicked")
     $('#landingPage').style('display: flex');
     $('#hungryPage').style('display: none');
     $('#thirstyPage').style('display: none');
@@ -21,10 +24,10 @@ function displayThirsty()    {
 function clear() {
     $("#cocktailResults").empty();
     //code to get ride of established search criteria and query add ons
-  }
+}
 
 // This is the beginnings of the ajax request, use #searchCocktails for the search button ID
-$('#searchCocktails').on('click', function(event)    {
+$('#searchCocktails').on('click', function (event) {
     event.preventDefault();
 
     // clear();
@@ -34,14 +37,14 @@ $('#searchCocktails').on('click', function(event)    {
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response)    {
-      console.log(response);
-      
-      });
+    }).then(function (response) {
+        console.log(response);
+
+    });
 });
 
 //These three IDs refer to the containers for the hungry / thirsty button&icon container 
 //and the landing button in the nav
-$('#hungry').on('click', displayHungry());
-$('#thirsty').on('click', displayThirsty());
-$('#landing').on('click', displayLanding());
+$('#hungry').on('click', displayHungry);
+$('#thirsty').on('click', displayThirsty);
+$('#landing').on('click', displayLanding);
